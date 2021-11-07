3d0d26d5bf7d22fe57253acb2e89556c4ea41e97
SzymonStolarski <szymonpiotrstolarski@gmail.com>
2021-11-07 15:44:37 +0100

    minor change in zad_a.py

--- zad_a.py
+++ zad_a.py
@@ -11,9 +11,17 @@ def print_names(list_of_names : list) -> None:
     -------
     None
     """
-    for name in list_of_names:
-        print(name)
+    if not len(list_of_names) == 5:
+        raise('List should be exact length of 5')
+
+    # one solution
+    # for name in list_of_names:
+    #     print(name)
+
+    # comprehension list solution
+
+    [print(name) for name in list_of_names]
 
 # Test
-names_list = ['andrzej', 'michal', 'wojciech']
+names_list = ['andrzej', 'michal', 'wojciech', 'arek', 'benek']
 print_names(names_list)
