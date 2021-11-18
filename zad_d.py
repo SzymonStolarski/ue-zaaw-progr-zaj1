c504a54453948cf2d2749921e9b4ab799727da05
SzymonStolarski <szymonpiotrstolarski@gmail.com>
2021-11-18 18:52:02 +0100

    Pycodestyle changes

--- zad_d.py
+++ zad_d.py
@@ -1,7 +1,8 @@
 # for loop
-for index, element in enumerate(range(1,11)):
-    if not index%2==0:
+for index, element in enumerate(range(1, 11)):
+    if not index % 2 == 0:
         print(element)
 
 # list comprehension
-[print(element) for index, element in enumerate(range(1,11)) if not index%2==0]
+[print(element) for index, element
+    in enumerate(range(1, 11)) if not index % 2 == 0]
